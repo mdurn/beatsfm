a9614373f123c7e08e231ecadffead4edd3c74d8
Michael Durnhofer <mdurnhofer@gmail.com>
2014-04-05 19:12:48 -0700

    production action_mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,7 @@ BeatsFm::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.eager_load = true
+
+  # Custom configurations
+    config.action_mailer.default_url_options = { host: 'beatsfm.herokuapp.com' }
 end
