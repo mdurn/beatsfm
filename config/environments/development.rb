094686c2975b8cedbfc69287f663a1965effe019
Michael Durnhofer <mdurnhofer@gmail.com>
2014-04-05 18:49:05 -0700

    additional configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ BeatsFm::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.eager_load = false
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
